1691e25e6c236618430c31a21bd6d2114f3a4a79
mahirkabir92 <mahirkabir92@gmail.com>
2023-09-27 16:21:17 -0400

    Lab 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+finchs = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finchs_index(request):
+  return render(request, 'finchs/index.html', {
+    'finchs': finchs
+  })
